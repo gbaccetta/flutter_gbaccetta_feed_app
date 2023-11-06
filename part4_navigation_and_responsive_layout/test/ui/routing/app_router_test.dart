2b47059cabd3790f1301746cf47271978b75b247
Giovanni Accetta <gbaccetta@gmail.com>
2023-11-06 09:08:13 -0400

    improvements to app_router_tests

--- part4_navigation_and_responsive_layout/test/ui/routing/app_router_test.dart
+++ part4_navigation_and_responsive_layout/test/ui/routing/app_router_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_gbaccetta_feed_app/core/locators/locator.dart';
 import 'package:flutter_gbaccetta_feed_app/domain/models/providers/article_list.dart';
 import 'package:flutter_gbaccetta_feed_app/domain/models/providers/user.dart';
 import 'package:flutter_gbaccetta_feed_app/ui/routing/app_router.dart';
+import 'package:flutter_gbaccetta_feed_app/ui/routing/nested_navigator.dart';
 import 'package:flutter_gbaccetta_feed_app/ui/routing/routes.dart';
 import 'package:flutter_gbaccetta_feed_app/ui/screens/_home/home_view.dart';
 import 'package:flutter_gbaccetta_feed_app/ui/screens/article_details/article_details_view.dart';
@@ -14,52 +15,69 @@ import 'package:provider/provider.dart';
 
 import '../../utils/test_utils.dart';
 
-class _FakeView extends StatefulWidget {
-  const _FakeView({super.key});
-  @override
-  State<StatefulWidget> createState() => _FakeViewState();
-}
-
-class _FakeViewState extends State<_FakeView> {
-  @override
-  Widget build(BuildContext context) => const SizedBox.expand();
-}
-
-abstract class MockView<T extends StatefulWidget> extends Mock {
+/// We want to be able to test navigation in the app without caring about the
+/// actual logic in each page. This will allow to tests all the url based
+/// navigation as well as deeplink and redirection without having to mock all
+/// the dependencies required by each page
+abstract class _MockView<T extends StatefulWidget> extends Mock {
+  final NestedNavigator? nestedNavigator;
+  _MockView({this.nestedNavigator});
   @override
   String toString({DiagnosticLevel? minLevel}) => super.toString();
-  StatefulElement createElement() => StatefulElement(_FakeView(
+
+  /// this method is what create the actual widget, hence we override it in our
+  /// mocked class to actually provide a fakeGoRouterPage that may contains a
+  /// nestedNavigator (such as the StatefulShellRoute that is our HomeView)
+  StatefulElement createElement() => StatefulElement(_FakeGoRouterPage(
         key: Key(runtimeType.toString()),
+        nestedNavigator: nestedNavigator,
       ));
-  State<T> createState() => _EmptyState();
 }
 
-class _EmptyState<T extends StatefulWidget> extends State<T> {
+/// This class is our actual Fake View containing only SizedBox or the nested
+/// navigatorContainer
+class _FakeGoRouterPage extends StatefulWidget {
+  final NestedNavigator? nestedNavigator;
+  const _FakeGoRouterPage({super.key, this.nestedNavigator});
   @override
-  Widget build(BuildContext context) => const SizedBox.expand();
+  State<StatefulWidget> createState() => _FakeGoRouterPageState();
 }
 
-class MockArticleListView extends MockView<ArticleListView>
+class _FakeGoRouterPageState extends State<_FakeGoRouterPage> {
+  @override
+  Widget build(BuildContext context) => SizedBox(
+        child: widget.nestedNavigator?.navigatorContainer ?? const SizedBox(),
+      );
+}
+
+/// Finally we need to mock our views. Note that we need to have the constructor
+/// of the MockHomeView mimic the real HomeView to let go_router actually pass
+/// the nested navigator informations.
+class _MockHomeView extends _MockView implements HomeView {
+  _MockHomeView({super.nestedNavigator});
+  @override
+  NestedNavigator get nestedNavigator => super.nestedNavigator!;
+}
+
+class _MockArticleListView extends _MockView<ArticleListView>
     implements ArticleListView {}
 
-class MockHomeView extends MockView implements HomeView {}
-
-class MockArticleDetailsView extends MockView<ArticleDetailsView>
+class _MockArticleDetailsView extends _MockView<ArticleDetailsView>
     implements ArticleDetailsView {}
 
 void main() {
   setUpAll(() {
-    serviceLocatorForTestInitialization();
-    getIt.unregister<ArticleListView>();
-    getIt.unregister<ArticleDetailsView>();
-    getIt.registerFactory<ArticleListView>(() => MockArticleListView());
-    getIt.registerFactory<ArticleDetailsView>(() => MockArticleDetailsView());
+    getIt.registerFactoryParam<HomeView, NestedNavigator, void>(
+        (nestedNav, _) => _MockHomeView(nestedNavigator: nestedNav));
+    getIt.registerFactory<ArticleListView>(() => _MockArticleListView());
+    getIt.registerFactory<ArticleDetailsView>(() => _MockArticleDetailsView());
   });
 
-  final homeView = find.byType(HomeView);
-  final articleListView = find.byKey(const Key('MockArticleListView'));
-  final articleDetailsView = find.byKey(const Key('MockArticleDetailsView'));
-  final userView = find.text('User');
+  //final homeView = find.byType(HomeView);
+  final homeView = find.byKey(const Key('_MockHomeView'));
+  final articleListView = find.byKey(const Key('_MockArticleListView'));
+  final articleDetailsView = find.byKey(const Key('_MockArticleDetailsView'));
+  final userView = find.text('User page');
 
   Future<void> init(
     WidgetTester tester, {
@@ -81,21 +99,24 @@ void main() {
     expect(homeView, findsOneWidget);
     expect(articleListView, findsOneWidget);
     expect(articleDetailsView, findsNothing);
+    expect(userView, findsNothing);
   });
 
   testWidgets('navigate to user view and back', (tester) async {
     await init(tester);
-    final BuildContext context = tester.element(homeView);
+    BuildContext context = tester.element(homeView);
     context.go(Routes.user());
     await tester.pumpAndSettle();
 
     expect(articleListView, findsNothing);
     expect(userView, findsOneWidget);
 
+    context = tester.element(homeView);
     context.go(Routes.articles());
     await tester.pumpAndSettle();
 
     expect(articleListView, findsOneWidget);
+    expect(userView, findsNothing);
   });
 
   testWidgets(
